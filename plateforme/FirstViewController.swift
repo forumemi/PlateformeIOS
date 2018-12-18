59b328698f83dd8a03d423f9f63f9b2ce408dfa3
BENTALEB ALI <BENTALEB@alis-mac-pro.local>
2018-12-18 19:46:26 +0100

    Initial Commit

---
+++ plateforme/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  plateforme
+//
+//  Created by BENTALEB ALI on 18/12/2018.
+//  Copyright © 2018 BENTALEB ALI. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ plateforme/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  plateforme
+//
+//  Created by BENTALEB ALI on 18/12/2018.
+//  Copyright © 2018 BENTALEB ALI. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
